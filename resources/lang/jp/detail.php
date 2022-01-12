2cb340c5ebb66dfd566c418723bbb2174950d3ba
phuong <dieuphuong272@example.com>
2022-01-12 10:00:02 +0700

    localization

---
+++ resources/lang/jp/detail.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'author' => "著者: ",
+]
+?>
---
+++ resources/lang/jp/home.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'home' => "ホーム",
+    'post' => "投稿",
+    'about' => "Bentory について",
+    'contact' => "コンタクト",
+    'theme' => "ベトナム料理",
+]
+?>
