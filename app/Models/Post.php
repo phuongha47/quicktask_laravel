3613fa04fabd0913f11ee9bbee9c39c98d438a55
phuong <dieuphuong272@example.com>
2022-01-12 16:05:26 +0700

    controller, model

---
+++ app/Models/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $table = "posts";
+    protected $primaryKey = 'id';
+    protected $guarded = [];
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class, 'post_author');
+    }
+}
